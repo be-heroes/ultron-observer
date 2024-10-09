baea93d08612ec80f78e5f28434944321c08cff4
Tobias Andersen <tobias.andersen79@gmail.com>
2024-10-09 22:37:48 +0200

    simple subscriber logic

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/be-heroes/ultron-observer
 
 go 1.23.1
+
+require (
+	github.com/be-heroes/ultron v0.0.8
+	github.com/redis/go-redis/v9 v9.6.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	ultron "github.com/be-heroes/ultron/pkg"
+
+	"github.com/redis/go-redis/v9"
+)
+
 const (
 	EnvironmentVariableKeyKubernetesConfig      = "KUBECONFIG"
 	EnvironmentVariableKeyKubernetesServiceHost = "KUBERNETES_SERVICE_HOST"
@@ -7,5 +19,38 @@ const (
 )
 
 func main() {
+	var redisClient *redis.Client
 
+	redisServerAddress := os.Getenv(ultron.EnvRedisServerAddress)
+	redisServerDatabase := os.Getenv(ultron.EnvRedisServerDatabase)
+	redisServerDatabaseInt, err := strconv.Atoi(redisServerDatabase)
+	if err != nil {
+		redisServerDatabaseInt = 0
+	}
+
+	if redisServerAddress != "" {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     redisServerAddress,
+			Password: os.Getenv(ultron.EnvRedisServerPassword),
+			DB:       redisServerDatabaseInt,
+		})
+
+		ctx := context.Background()
+
+		_, err := redisClient.Ping(ctx).Result()
+		if err != nil {
+			log.Fatalf("Failed to ping redis server with error: %v", err)
+		}
+
+		pubsub := redisClient.Subscribe(ctx, ultron.TopicPodObserve, ultron.TopicNodeObserve)
+		defer pubsub.Close()
+
+		ch := pubsub.Channel()
+
+		fmt.Println("Subscribed to channels. Waiting for messages...")
+
+		for msg := range ch {
+			fmt.Printf("Received message from %s: %s\n", msg.Channel, msg.Payload)
+		}
+	}
 }
