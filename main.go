1ccd836a90335d14abd573c22f8996bccb773cf7
Tobias Andersen <tobias.andersen79@gmail.com>
2024-10-10 08:10:24 +0200

    ad-hoc refaq

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -10,9 +11,12 @@ import (
 	ultron "github.com/be-heroes/ultron/pkg"
 
 	"github.com/redis/go-redis/v9"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func main() {
+	log.Println("Initializing ultron-observer")
+
 	var redisClient *redis.Client
 
 	redisServerAddress := os.Getenv(ultron.EnvRedisServerAddress)
@@ -22,6 +26,8 @@ func main() {
 		redisServerDatabaseInt = 0
 	}
 
+	ctx := context.Background()
+
 	if redisServerAddress != "" {
 		redisClient = redis.NewClient(&redis.Options{
 			Addr:     redisServerAddress,
@@ -29,22 +35,45 @@ func main() {
 			DB:       redisServerDatabaseInt,
 		})
 
-		ctx := context.Background()
-
 		_, err := redisClient.Ping(ctx).Result()
 		if err != nil {
 			log.Fatalf("Failed to ping redis server with error: %v", err)
 		}
+	}
 
+	log.Println("Initialized ultron-observer")
+
+	if redisClient != nil {
 		pubsub := redisClient.Subscribe(ctx, ultron.TopicPodObserve, ultron.TopicNodeObserve)
 		defer pubsub.Close()
 
 		ch := pubsub.Channel()
 
-		fmt.Println("Subscribed to channels. Waiting for messages...")
+		fmt.Println("Subscribed to redis channels. Waiting for messages...")
 
 		for msg := range ch {
-			fmt.Printf("Received message from %s: %s\n", msg.Channel, msg.Payload)
+			switch msg.Channel {
+			case ultron.TopicPodObserve:
+				var pod corev1.Pod
+
+				err := json.Unmarshal([]byte(msg.Payload), &pod)
+				if err != nil {
+					log.Fatalf("Error deserializing msg.Payload to Pod: %v", err)
+				}
+
+				// TODO: Implement logic to handle Pod events
+			case ultron.TopicNodeObserve:
+				var node corev1.Node
+
+				err := json.Unmarshal([]byte(msg.Payload), &node)
+				if err != nil {
+					log.Fatalf("Error deserializing msg.Payload to Node: %v", err)
+				}
+
+				// TODO: Implement logic to handle Node events
+			default:
+				fmt.Printf("Received message from unsupported channel: %s\n", msg.Channel)
+			}
 		}
 	}
 }
